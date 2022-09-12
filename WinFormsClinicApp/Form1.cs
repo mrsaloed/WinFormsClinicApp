6682b250f7b1a7ad3368b90f344bc7e3371da15b
Mikhail Leonov <mikhail.leonov62@gmail.com>
2022-09-12 10:02:27 +0300

    Добавьте файлы проекта.

---
+++ WinFormsClinicApp/Form1.cs
@@ -0,0 +1,14 @@
+using MaterialSkin.Controls;
+
+namespace WinFormsClinicApp
+{
+    public partial class MainForm : MaterialForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
